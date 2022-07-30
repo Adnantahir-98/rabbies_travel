0a98a72d78316291c6dbd57d56039020139f9b39
Adnantahir-98 <adnantahir1998@gmail.com>
2022-07-30 13:23:28 +0500

    Tour page

---
+++ src/Screens/Tours.js
@@ -0,0 +1,29 @@
+import {Container, Row, Col} from 'react-bootstrap'
+
+
+const Tours = () => {
+  return (
+    <div>
+        <section className='bg-about-img'>
+        <code>
+          <h1 className='text-white text-center' style={{paddingTop: '200px', textShadow: '1px 2px #000'}}>
+            Loch Ness, Glencoe & the Highlands
+          </h1>
+          <h5>1 day tour</h5>
+        </code>             
+      </section>
+
+
+        <section>
+            <Container>
+                <Row>
+                    Tour Page
+                </Row>
+            </Container>
+        </section>
+
+    </div>
+  )
+}
+
+export default Tours
