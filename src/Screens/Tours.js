1e48a49204175add61ce8ad5f11dc0443190619a
Adnantahir-98 <adnantahir1998@gmail.com>
2022-07-30 13:30:08 +0500

    TOur page added

--- src/Screens/Tours.js
+++ src/Screens/Tours.js
@@ -17,7 +17,7 @@ const Tours = () => {
         <section>
             <Container>
                 <Row>
-                    Tour Page
+                    <h1>Tour Page</h1>
                 </Row>
             </Container>
         </section>
